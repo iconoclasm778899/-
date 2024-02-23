1b2c711971c1fb7410a76beee6760427d80c232d
Cosmo <Cosmo@PC>
2024-02-23 19:16:47 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsForms sample/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsForms_sample
+{
+    partial class frm_memo
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.フォルダToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.書式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.赤ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.赤ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.黒ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(800, 412);
+            this.textBox1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.フォルダToolStripMenuItem,
+            this.書式ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 38);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // フォルダToolStripMenuItem
+            // 
+            this.フォルダToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くToolStripMenuItem,
+            this.保存ToolStripMenuItem});
+            this.フォルダToolStripMenuItem.Name = "フォルダToolStripMenuItem";
+            this.フォルダToolStripMenuItem.Size = new System.Drawing.Size(88, 34);
+            this.フォルダToolStripMenuItem.Text = "ファイル";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(158, 38);
+            this.開くToolStripMenuItem.Text = "開く";
+            this.開くToolStripMenuItem.Click += new System.EventHandler(this.開くToolStripMenuItem_Click);
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(158, 38);
+            this.保存ToolStripMenuItem.Text = "保存";
+            this.保存ToolStripMenuItem.Click += new System.EventHandler(this.保存ToolStripMenuItem_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "テキストファイル(*.txt)|*.txt";
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.ShowNewFolderButton = false;
+            // 
+            // 書式ToolStripMenuItem
+            // 
+            this.書式ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.赤ToolStripMenuItem});
+            this.書式ToolStripMenuItem.Name = "書式ToolStripMenuItem";
+            this.書式ToolStripMenuItem.Size = new System.Drawing.Size(71, 34);
+            this.書式ToolStripMenuItem.Text = "設定";
+            // 
+            // 赤ToolStripMenuItem
+            // 
+            this.赤ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.黒ToolStripMenuItem,
+            this.赤ToolStripMenuItem1});
+            this.赤ToolStripMenuItem.Name = "赤ToolStripMenuItem";
+            this.赤ToolStripMenuItem.Size = new System.Drawing.Size(270, 38);
+            this.赤ToolStripMenuItem.Text = "色";
+            // 
+            // 赤ToolStripMenuItem1
+            // 
+            this.赤ToolStripMenuItem1.Name = "赤ToolStripMenuItem1";
+            this.赤ToolStripMenuItem1.Size = new System.Drawing.Size(270, 38);
+            this.赤ToolStripMenuItem1.Text = "赤";
+            this.赤ToolStripMenuItem1.Click += new System.EventHandler(this.赤ToolStripMenuItem1_Click);
+            // 
+            // 黒ToolStripMenuItem
+            // 
+            this.黒ToolStripMenuItem.Name = "黒ToolStripMenuItem";
+            this.黒ToolStripMenuItem.Size = new System.Drawing.Size(270, 38);
+            this.黒ToolStripMenuItem.Text = "黒";
+            this.黒ToolStripMenuItem.Click += new System.EventHandler(this.黒ToolStripMenuItem_Click);
+            // 
+            // frm_memo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frm_memo";
+            this.Text = "memo";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem フォルダToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripMenuItem 書式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 赤ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 赤ToolStripMenuItem1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ToolStripMenuItem 黒ToolStripMenuItem;
+    }
+}
+
---
+++ WindowsForms sample/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsForms_sample
+{
+    public partial class frm_memo : Form
+    {
+        public frm_memo()
+        {
+            InitializeComponent();
+        }
+
+        private void 開くToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "テキストファイル(*.txt)|*.txt";
+            dialog.Title = "開く";
+            if (dialog.ShowDialog() == DialogResult.OK)
+                textBox1.Text = File.ReadAllText(dialog.FileName);
+
+        }
+
+        private void 保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "テキストファイル(*.txt)|*.txt";
+            dialog.Title = "保存";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(dialog.FileName, textBox1.Text);
+            }
+        }
+
+        private void 赤ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //テキストボックス内全て色が変わっている！！
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void 黒ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = Color.Black;
+        }
+    }
+    
+}
+
